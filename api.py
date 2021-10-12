34277767cf525d802313dc810a170ef4bd0b8bbe
BangBarMau <74715623+BangBarMau@users.noreply.github.com>
2021-10-12 20:40:21 +0800

    prototype for flasgger documentation posting a book not working

--- api.py
+++ api.py
@@ -1,9 +1,15 @@
 from flask import Flask
 from flask_restful import reqparse, abort, Api, Resource
 from flask_ngrok import run_with_ngrok
+from flasgger import Swagger
 
 app = Flask(__name__)
 api = Api(app)
+app.config['SWAGGER'] = {
+    'title': 'Flasgger RESTful',
+    'uiversion': 2
+}
+swagger = Swagger(app)
 run_with_ngrok(app)
 
 
@@ -21,42 +27,110 @@ parser.add_argument('title')
 parser.add_argument('author')
 
 
-# Todo
-# shows a single todo item and lets you delete a todo item
+
 class Book(Resource):
     def get(self, book_id):
+        """
+                Get a specific book
+                ---
+                tags:
+                  - restful
+                parameters:
+                  - in: path
+                    name: book_id
+                    required: true
+                    description: The list of the book.
+                    type: string
+                    examples: book0
+                responses:
+                  200:
+                    description: Book List
+                    schema:
+                      id: Book
+                      properties:
+                        book:
+                          type: string
+                          default:
+                """
         abort_if_todo_doesnt_exist(book_id)
         return BOOKS[book_id]
 
     def delete(self, book_id):
+        """
+               Delete a book
+               ---
+               tags:
+                 - restful
+               parameters:
+                 - in: path
+                   name: book_id
+                   required: true
+                   description: The ID of the book
+                   type: string
+               responses:
+                 204:
+                   description: Book deleted
+               """
         abort_if_todo_doesnt_exist(book_id)
         del BOOKS[book_id]
         return '', 204
 
     def put(self, book_id):
+        """
+               Update a book's information
+               ---
+               tags:
+                 - restful
+               parameters:
+                 - in: body
+                   name: body
+                   schema:
+                     $ref: '#/definitions/Task'
+                 - in: path
+                   name: todo_id
+                   required: true
+                   description: The ID of the book
+                   type: string
+               responses:
+                 201:
+                   description: The information has been updated
+                   schema:
+                     $ref: '#/definitions/Book'
+               """
         args = parser.parse_args()
-        book = {'task': args['task']}
+        book = {'title': args['title'], 'author': args['author']}
         BOOKS[book_id] = book
         return book, 201
 
 
-# TodoList
-# shows a list of all todos, and lets you POST to add new tasks
 class BooksList(Resource):
     def get(self):
         return BOOKS
 
     def post(self):
+        """
+               Enter book data
+               ---
+               tags:
+                 - restful
+               parameters:
+                 - in: body
+                   name: body
+                   schema:
+                     $ref: '#/definitions/Book'
+               responses:
+                 201:
+                   description: The book information has been entered
+                   schema:
+                     $ref: '#/definitions/Book'
+               """
         args = parser.parse_args()
         book_id = int(max(BOOKS.keys()).lstrip('book')) + 1
         book_id = 'book%i' % book_id
         BOOKS[book_id] = {'title': args['title'], 'author': args['author']}
-        #'book0': {'title': 'placeholder'},
         return BOOKS[book_id], 201
 
-##
-## Actually setup the Api resource routing here
-##
+
 api.add_resource(BooksList, '/books')
 api.add_resource(Book, '/books/<book_id>')
 
